482bbc8cdcba407440b70f57feda9b588137a71d
theuFaria <mdsfaria2025wt@gmail.com>
2025-10-14 17:25:02 -0300

    commit init

---
+++ README.md
@@ -0,0 +1,8 @@
+<h1>Meu Portifólio Pessoal</h1>
+<p style="font-size: 16px">Meu portifólio de apresentação pessoal, contendo meu projetos, falando sobre mim e quais as minhas habilidades.</p>
+<h2>Tecnologias usadas: <h2>
+<img  style="width:50px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" />
+<img  style="width:50px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" />
+<img  style="width:50px;" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jquery/jquery-original.svg" />
+<h2>Veja aqui:</h2>
+<a href="https://theufaria.github.io/Portifolio-pessoal" target="_blank">Link</a>
---
+++ arquivo-js/script.js
@@ -0,0 +1,40 @@
+const projects = $(".projeto");
+const overlay = $(".overlay");
+
+projects.each(function (i) {
+    $(this).on("mouseenter", () => {
+        overlay.eq(i).addClass("animationDragUp");
+        overlay.eq(i).removeClass("hide");
+        setTimeout(() => overlay.eq(i).removeClass("aniamtionDragUp"), 300)
+        iconsLinksAppear();
+    })
+})
+
+projects.each(function (i) {
+    $(this).on("mouseleave", () => {
+        overlay.eq(i).addClass("animationDragDown");
+        iconsLinksDesapear();
+        setTimeout(() => {
+            overlay.eq(i).addClass("hide");
+            overlay.eq(i).removeClass("animationDragDown");
+        }, 280)
+    })
+});
+
+const linksProject = $(".linksProject");
+function iconsLinksAppear() {
+    linksProject.each(function (i) {
+        linksProject.eq(i).addClass("actived");
+    })
+};
+function iconsLinksDesapear() {
+    linksProject.filter(() => linksProject.hasClass("actived")).removeClass("actived");
+}
+
+$(window).on("resize", function () {
+    if ($(window).width() < 500) {
+        $("#welcome").html("Eu sou o Matheus Faria <br> <span>Seja bem vindo ao meu portifólio website!</span>");
+    } else if ($(window).width() > 500) {
+        $("#welcome").html("Eu sou o Matheus Faria, <br> <span>Seja bem vindo ao meu portifólio website!</span>");
+    }
+})
